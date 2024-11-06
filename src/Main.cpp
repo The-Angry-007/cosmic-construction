b0a52e24de2bc689e8127c5cd1b84c2e1e5c8592
kieran <46075444+The-Angry-007@users.noreply.github.com>
2024-11-06 09:41:35 +0000

    reverted back to blank window

--- src/Main.cpp
+++ src/Main.cpp
@@ -1,103 +1,30 @@
-#include "Include.hpp"
-#ifndef M_PI
-	#define M_PI 3.14159265358979323846
-#endif
+#include "Main.hpp"
+#include <memory>
 
-#include <math.h>
-
-sf::RenderWindow* window = new sf::RenderWindow();
-sf::Clock deltaClock;
-//Gui gui;
-InputHandler inp;
-sf::Font* defaultFont = new sf::Font();
-sf::Font* pixelFont = new sf::Font();
+//a unique pointer to the window object; this is unique to prevent accidentally creating multiple windows
+std::unique_ptr<sf::RenderWindow> window;
+//width and height of the window
 int width, height;
-Menu menu;
-Game* game = nullptr;
-bool debug = false;
-Timer timer;
-Camera camera(sf::Vector2f(0.f, 0.f), 1.f);
+
+//the main procedure that runs the program
 int main()
 {
-	SaveHandler::ResetDir();
-	util::Platform platform;
-	// in Windows at least, this must be called before creating the window
-	float screenScalingFactor = platform.getScreenScalingFactor(window->getSystemHandle());
-	// Use the screenScalingFactor
-	window->create(sf::VideoMode(800.0f * screenScalingFactor, 500.0f * screenScalingFactor), "Cosmic Construction");
-	// window->setVerticalSyncEnabled(false);
-	window->setFramerateLimit(200);
+	//initialise the window object
+	window = std::make_unique<sf::RenderWindow>();
+	//make a 800x500 window with the title "Cosmic Construction"
+	window->create(sf::VideoMode(800.0f, 500.0f), "Cosmic Construction");
+	//set the icon image that is displayed in the corner of the window
 	sf::Image icon;
 	icon.loadFromFile("resources/images/icon.png");
 	window->setIcon(256, 256, icon.getPixelsPtr());
-	if (!defaultFont->loadFromFile("resources/fonts/RobotoMono-Regular.ttf"))
-	{
-		window->close();
-	}
-	if (!pixelFont->loadFromFile("resources/fonts/pixel.ttf"))
-	{
-		window->close();
-	}
-
-	//defaultFont = pixelFont;
-	//gui.Init();
-
-	InitItems();
-	menu = InitMenu();
-	camera.Update();
-	deltaClock.restart();
-	if (debug)
-	{
-		menu.OpenMenu(3);
-		menu.slots[0].onClick();
-	}
 	while (window->isOpen())
 	{
-		timer.TimeFunc("whole frame", false);
-		timer.TimeFunc("whole update", true);
-		timer.TimeFunc("input events", true);
-		inp.ProcessEvents();
-		timer.end();
-		timer.nFrames++;
-
-		double dt = deltaClock.restart().asSeconds();
-
-		if (game != nullptr)
+		sf::Event event;
+		while (window->pollEvent(event))
 		{
-			game->Update(dt);
-		}
-		timer.TimeFunc("camera update", true);
-		camera.Update();
-		timer.end();
-		timer.TimeFunc("menu update", true);
-		menu.Update(dt);
-		timer.end();
-		timer.end();
-		timer.TimeFunc("whole render", false);
-		window->clear(sf::Color(8, 0, 20));
-		timer.TimeFunc("game render", false);
-		if (game != nullptr)
-		{
-			game->Render();
-		}
-		timer.end();
-		timer.TimeFunc("render menu", false);
-		camera.RenderMenu(&menu, dt);
-		timer.end();
-		timer.TimeFunc("display window", false);
-		window->display();
-		timer.end();
-		timer.end();
-		timer.end();
-		if (inp.keyDown(sf::Keyboard::LControl) && inp.keyPressed(sf::Keyboard::Key::T))
-		{
-			if (!timer.timing)
+			if (event.type == event.Closed)
 			{
-				timer.StartTiming();
-			}
-			else
-			{
-				timer.EndTiming();
+				window->close();
 			}
 		}
 	}
--- src/Main.hpp
+++ src/Main.hpp
@@ -1,15 +1,3 @@
 #pragma once
-#include "Include.hpp"
-class Timer;
-class InputHandler;
-extern sf::RenderWindow* window;
-//the constant that determines the strength of gravity
-extern InputHandler inp;
-//extern Gui gui;
+extern std::unique_ptr<sf::RenderWindow> window;
 extern int width, height;
-extern sf::Font* defaultFont;
-extern sf::Font* pixelFont;
-extern Menu menu;
-extern Game* game;
-extern Camera camera;
-extern Timer timer;
