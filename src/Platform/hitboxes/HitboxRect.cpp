5476074a8f3cfbff3646007cb26a6c8ddd395b6d
kieran <46075444+The-Angry-007@users.noreply.github.com>
2024-11-07 09:55:54 +0000

    added draw function

--- src/Platform/hitboxes/HitboxRect.cpp
+++ src/Platform/hitboxes/HitboxRect.cpp
@@ -1,4 +1,5 @@
 #include "HitboxRect.hpp"
+#include "Main.hpp"
 #include "utils.hpp"
 HitboxRect::HitboxRect(sf::Vector2f position, sf::Vector2f size)
 {
@@ -16,9 +17,19 @@ bool HitboxRect::intersects(HitboxShape* other)
 {
 	if (other->index == 0)
 	{
-		sf::FloatRect rect1(currentPos, currentSize);
-		sf::FloatRect rect2(other->currentPos, other->currentSize);
+		//float rect requires top left corner and total width and height,
+		//pos and size are centre position and half of total width and height
+		sf::FloatRect rect1(currentPos - currentSize, currentSize * 2.f);
+		sf::FloatRect rect2(other->currentPos - other->currentSize, other->currentSize * 2.f);
 		return RectIntersectsRect(rect1, rect2);
 	}
 	return false;
+}
+
+void HitboxRect::Display()
+{
+	sf::RectangleShape drawRect(currentSize * 2.f);
+	drawRect.setPosition(currentPos);
+	drawRect.setOrigin(currentSize);
+	window->draw(drawRect);
 }
--- src/Platform/hitboxes/HitboxRect.hpp
+++ src/Platform/hitboxes/HitboxRect.hpp
@@ -7,4 +7,7 @@ public:
 	HitboxRect(sf::Vector2f position, sf::Vector2f size);
 	~HitboxRect();
 	bool intersects(HitboxShape* other);
+
+	//temporary function to test
+	void Display();
 };
