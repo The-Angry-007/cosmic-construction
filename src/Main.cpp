9fbcdd861c102837d46b4ed45b3025bedaf7dc25
kieran <46075444+The-Angry-007@users.noreply.github.com>
2024-11-08 12:39:37 +0000

    removed circles test

--- src/Main.cpp
+++ src/Main.cpp
@@ -22,20 +22,11 @@ int main()
 	icon.loadFromFile("resources/images/icon.png");
 	window->setIcon(256, 256, icon.getPixelsPtr());
 
-	std::vector<HitboxShape*> shapes;
-	shapes.push_back(new HitboxCircle(sf::Vector2f(0.f, 0.f), 40.f));
-	shapes.push_back(new HitboxRect(sf::Vector2f(200.f, 100.f), sf::Vector2f(50.f, 50.f)));
-	shapes.push_back(new HitboxCircle(sf::Vector2f(450.f, 200.f), 200.f));
 	while (window->isOpen())
 	{
 		InputHandler::ProcessEvents();
 
 		window->clear(sf::Color::Black);
-		shapes[0]->SetTransform(InputHandler::mousePos, sf::Vector2f(1.f, 1.f));
-		for (uint i = 0; i < shapes.size(); i++)
-		{
-			shapes[i]->Display(shapes);
-		}
 		window->display();
 	}
 	return 0;
