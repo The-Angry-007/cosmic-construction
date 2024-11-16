d5e22554b90d39ae09e2b48f2bad1d4cd05016f7
kieran <46075444+The-Angry-007@users.noreply.github.com>
2024-11-16 20:23:26 +0000

    half finished load game menu
    cant add save slots yet cuz i have no idea how to make them cut off at the top and bottom of the panel.
    best idea is probably to render them to a render texture then crop that?

--- src/gui/GUIHandler.cpp
+++ src/gui/GUIHandler.cpp
@@ -164,7 +164,17 @@ void GUIHandler::InitGUIS()
 		g->AddObject(dimpanel);
 		//label at the top of the screen that says "load game"
 		GUILabel* newgamelabel = new GUILabel(sf::Vector2f(0.5f, 0.05f), sf::Vector2f(0.5f, 0.03f), "Load Game");
-
+		//big box with save slots in
+		GUIPanel* bg = new GUIPanel(sf::Vector2f(0.5f, 0.5f), sf::Vector2f(0.4f, 0.4f), sf::Color(189, 189, 189));
+		g->AddObject(bg);
+		//header
+		float headerHeight = 0.04f;
+		GUIPanel* headerPanel = new GUIPanel(sf::Vector2f(0.5f, 0.1f + headerHeight), sf::Vector2f(0.4f, headerHeight), sf::Color(215, 215, 215));
+		GUILabel* headerText = new GUILabel(sf::Vector2f(0.3f, 0.1f + headerHeight), sf::Vector2f(0.2f, headerHeight - 0.01f), "Save Files");
+		headerText->SetColor(sf::Color::Black);
+		headerText->origin = sf::Vector2f(0.f, 0.5f);
+		g->AddObject(headerPanel);
+		g->AddObject(headerText);
 		guis.push_back(g);
 	}
 	//SETTINGS MENU
