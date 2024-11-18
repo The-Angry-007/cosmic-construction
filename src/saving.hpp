85a23a65d6584701e26284f75fb3754035af8a15
kieran <46075444+The-Angry-007@users.noreply.github.com>
2024-11-18 17:19:36 +0000

    accidentally put code in the wrong file

--- src/saving.hpp
+++ src/saving.hpp
@@ -1,25 +1,2 @@
 #pragma once
 #include "saving/JSON.hpp"
-JSON::JSON()
-{
-	keywords = {};
-	values = {};
-}
-
-std::string JSON::GetValue(std::string keyword)
-{
-	for (uint i = 0; i < keywords.size(); i++)
-	{
-		if (keywords[i] == keyword)
-		{
-			return values[i];
-		}
-	}
-	return "not in list";
-}
-
-void JSON::AddAttribute(std::string keyword, std::string value)
-{
-	keywords.push_back(keyword);
-	values.push_back(value);
-}
