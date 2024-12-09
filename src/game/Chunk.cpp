1d60aa0a8df3f227c54710dc67a66fb34fa1dc1d
kieran <46075444+The-Angry-007@users.noreply.github.com>
2024-12-09 17:50:54 +0000

    made chunks draw their positions

--- src/game/Chunk.cpp
+++ src/game/Chunk.cpp
@@ -29,9 +29,16 @@ void Chunk::Update(float dt)
 		items[i].Update(dt);
 	}
 }
-
+sf::Vector2f Chunk::GetWorldPos(sf::Vector2f position)
+{
+	return ((sf::Vector2f)position) * (float)CHUNK_SIZE * (float)TILE_SIZE + position;
+}
 void Chunk::Render()
 {
+	sf::RectangleShape chunkRect(sf::Vector2f(CHUNK_SIZE * TILE_SIZE, CHUNK_SIZE * TILE_SIZE));
+	chunkRect.setFillColor(sf::Color(abs(position.x) * 25, abs(position.y) * 25, 0));
+	chunkRect.setPosition(GetWorldPos(sf::Vector2f(0, 0)));
+	window->draw(chunkRect);
 	for (int i = 0; i < items.size(); i++)
 	{
 		items[i].Render();
